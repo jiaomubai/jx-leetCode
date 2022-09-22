8228bace12cc3ca627b6b96c244240c8daddaa6a
jiaoxian <1923419656@qq.com>
2022-09-22 18:15:45 +0800

    leetCode-剑指 Offer II 025: 链表中的两数相加

--- leetcode-medium/src/main/java/addTwoNumbers/AddTwoNumbers.java
+++ leetcode-medium/src/main/java/addTwoNumbers/AddTwoNumbers.java
@@ -2,6 +2,9 @@ package addTwoNumbers;
 
 import util.ListNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @ClassName: AddTwoNumbers
  * @Description: leetCode-2: 两数相加
@@ -19,7 +22,6 @@ public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode listNode1, ListNode listNode2) {
         // 预先指针
         ListNode pre = new ListNode(0);
-        ListNode.displayList(pre);
         // 当前指针
         ListNode cur = pre;
         // 进位
@@ -46,6 +48,36 @@ public class AddTwoNumbers {
         return pre.next;
     }
 
+    // leetCode-剑指 Offer II 025: 链表中的两数相加
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        Deque<ListNode> dequeL1 = new ArrayDeque<>();
+        Deque<ListNode> dequeL2 = new ArrayDeque<>();
+        // l1 和 l2 分别入栈, 则栈顶元素就是原链表中的尾节点
+        while(l1 != null) {
+            dequeL1.addLast(l1);
+            l1 = l1.next;
+        }
+        while(l2 != null) {
+            dequeL2.addLast(l2);
+            l2 = l2.next;
+        }
+
+        ListNode temp = null;
+        int carry = 0;
+        while(!dequeL1.isEmpty() || !dequeL2.isEmpty() || carry != 0) {
+            // 遍历两个栈, 进行元素的加法操作以及栈的弹栈操作
+            int val1 = dequeL1.isEmpty() ? 0 : dequeL1.removeLast().val;
+            int val2 = dequeL2.isEmpty() ? 0 : dequeL2.removeLast().val;
+            ListNode node = new ListNode((val1 + val2 + carry) % 10);
+            if (temp != null) {
+                node.next = temp;
+            }
+            temp = node;
+            carry = (val1 + val2 + carry) / 10;
+        }
+        return temp;
+    }
+
     public static void main(String[] args) {
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
         int[] nums1 = {2, 4, 3};
@@ -53,7 +85,15 @@ public class AddTwoNumbers {
         ListNode listNode1 = ListNode.createList(nums1);
         ListNode listNode2 = ListNode.createList(nums2);
         ListNode listNode3 = addTwoNumbers.addTwoNumbers(listNode1, listNode2);
+        System.out.println("result1 = ");
         ListNode.displayList(listNode3);
+
+        ListNode listNode11 = ListNode.createList(new int[]{7, 2, 4, 3});
+        ListNode listNode21 = ListNode.createList(new int[]{5, 6, 4});
+        ListNode listNode4 = addTwoNumbers.addTwoNumbers2(listNode11, listNode21);
+        System.out.println("result2 = ");
+        ListNode.displayList(listNode4)
+        ;
     }
 
 }
